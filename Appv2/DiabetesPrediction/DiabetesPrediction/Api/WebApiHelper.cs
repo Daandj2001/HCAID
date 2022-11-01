672a79ee3d5b7ea4a1ee3a4cc991da2ebe8f7b02
Daan <daandj2001@gmail.com>
2022-11-01 16:23:32 +0100

    edit api url

--- Appv2/DiabetesPrediction/DiabetesPrediction/Api/WebApiHelper.cs
+++ Appv2/DiabetesPrediction/DiabetesPrediction/Api/WebApiHelper.cs
@@ -14,15 +14,15 @@ namespace DiabetesPrediction.Api
 
             _httpClient = new HttpClient();
             _httpClient.BaseAddress =
-                new Uri("http://localhost:5000/api"); /// meshien nog /api achter de uri
+                new Uri("http://206.189.8.200:4999/api"); /// meshien nog /api achter de uri
             _httpClient.DefaultRequestHeaders.Accept.Clear();
             _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
         public async Task<Prediction> PredictionGet(PredictionModel predictionModel)
         {
-            //http://206.189.8.200:4999/api
-            HttpResponseMessage response = await _httpClient.PostAsJsonAsync("http://localhost:5000/api", predictionModel);
+            //http://localhost:5000/api
+            HttpResponseMessage response = await _httpClient.PostAsJsonAsync("http://206.189.8.200:4999/api", predictionModel);
             if (response.IsSuccessStatusCode)
             {
                 var result = await response.Content.ReadAsStringAsync();
